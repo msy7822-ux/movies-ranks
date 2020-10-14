56d400cabf43564e055afc12d5f3c6da36f53feb
msy7822-ux <masa.1108@icloud.com>
2020-10-15 00:28:37 +0900

    naore

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   before_action :authemnticate?, only: [:new, :create]
 
   def index
-    @movies = Movie.all.page(params[:page]).per(6).order(points: "DESC", :created_at)
+    @movies = Movie.all.page(params[:page]).per(6).order(:points, :id)
   end
 
   def show
