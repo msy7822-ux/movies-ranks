9cc7a68127d05f81e211905563d567ac95a141ab
msy7822-ux <masa.1108@icloud.com>
2020-10-16 00:50:05 +0900

    add Movie.where(id: vote_arr.pluck(:movie_id))

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
         # ユーザーが投票ないしは、コメントした映画情報を格納する配列
         movies = []
         vote_arr.each do |vote|
-            movies << Movie.find(vote.movie_id)
+            movies << Movie.where(id: vote_arr.pluck(:movie_id))
         end
         return movies
     end
