7e85c6f02ac30ff21ce5d6356201c93bbbea99bd
msy7822-ux <masa.1108@icloud.com>
2020-10-15 19:40:47 +0900

    change root position

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'homes#home'
   resources :votes, only: [:new, :create]
   resources :movies
 
@@ -10,6 +11,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   resources :users
 
-  root to: 'homes#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
