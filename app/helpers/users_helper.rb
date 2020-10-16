e5456c871070ed664b77e981051a16b718c3bda7
msy7822-ux <masa.1108@icloud.com>
2020-10-16 10:49:24 +0900

    change a little

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,6 @@
 module UsersHelper
     def movies(vote_arr)
         # ユーザーが投票ないしは、コメントした映画情報を格納する配列
-        # movies = vote_arr.pluck(:movie_id)
-        movies = Movie.where(id: vote_arr.pluck(:movie_id))
-        return movies
+        return Movie.where(id: vote_arr.pluck(:movie_id))
     end
 end
